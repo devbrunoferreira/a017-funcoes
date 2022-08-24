a213d82cd3c4520e467b228a4359c165e8572c99
Bruno Ferreira <bgsf1991@gmail.com>
2022-08-23 21:31:21 -0300

    Modificação na resolução dos Exercícios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,8 +1,9 @@
-//////////////// EXERCÍCIO I ////////////////
+////////////////// EXERCÍCIO I ////////////////
 
 console.log("////////////////// EXERCÍCIO 1 /////////////////////");
 console.log("");
 console.log("a) Declare uma função que imprima Olá, [SEU NOME]!. Chame esta função.");
+
 function meuNome(nome) {
     console.log(`Olá, ${nome}!`);
 }
@@ -11,6 +12,7 @@ meuNome("Bruno");
 
 console.log('');
 console.log("b) Declare uma função que imprima a tabuada do 6. Chame esta função.");
+
 const tabuada = function (numero) {
     for (let i = 1; i <= 10; i++) {
         console.log(`${i} x ${numero} = ${i * numero}`);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -3,24 +3,27 @@ console.log("");
 //a) Uma função que receba 2 números como parâmetros, e, dentro da função, faça a soma das 
 //duas entradas e imprima o resultado.Invoque a função e imprima no console o resultado.
 console.log("a) Uma função que receba 2 números como parâmetros, e, dentro da função, faça a soma das duas entradas e imprima o resultado. Invoque a função e imprima no console o resultado.");
+
 function soma(numero1, numero2) {
-    let soma = numero1 + numero2;
-    console.log(`- ${numero1} + ${numero2} = ${soma}`);
+    const soma = numero1 + numero2;
+    // console.log(`- ${numero1} + ${numero2} = ${soma}`);
     return soma;
 }
 
-soma(4, 6);
+const resultado = soma(4, 6);
+console.log(resultado);
 
 console.log("");
 // b) Uma função que recebe 2 números e imprime um booleano que informa se o 
 // primeiro número é maior ou igual ao segundo.
 console.log("b) Uma função que recebe 2 números e imprime um booleano que informa se o primeiro número é maior ou igual ao segundo.");
-let numeroMaior = (numero1, numero2) => {
+
+const numeroMaior = (numero1, numero2) => {
     if (numero1 >= numero2) {
-        let maior = numero1 >= numero2;
+        const maior = numero1 >= numero2;
         console.log(`- O numero ${numero1} é >= a ${numero2}? - (${maior})`);
     } else {
-        let maior = numero1 >= numero2;
+        const maior = numero1 >= numero2;
         console.log(`- O numero ${numero1} é >= a ${numero2}? - (${maior})`);
     }
 }
@@ -30,13 +33,16 @@ console.log("");
 
 // c) Uma função que receba um número e imprima se ele é par ou não
 console.log("c) Uma função que receba um número e imprima se ele é par ou não.");
-let parOuImpar = function (numero) {
+// SOLUÇÃO 1
+const parOuImpar = function (numero) {
     if (numero % 2 === 0) {
         console.log(`- O número ${numero} é par.`);
     } else {
         console.log(`- O número ${numero} é ímpar.`);
     }
 }
+// SOLUÇÃO 2
+// const parOuImpar = numero => { return console.log(`Seu numero é par? R: ${numero % 2 === 0}`); }
 
 parOuImpar(10);
 console.log("");
@@ -44,8 +50,10 @@ console.log("");
 // d) Uma função que recebe uma mensagem (string) como parâmetro e imprima o tamanho
 // dessa mensagem, juntamente com uma versão dela contendo apenas letras maiúsculas.
 console.log("d) Uma função que recebe uma mensagem (string) como parâmetro e imprima o tamanho dessa mensagem, juntamente com uma versão dela contendo apenas letras maiúsculas.");
+
 const mensagemString = (mensagem) => {
     console.log(`- ${mensagem.length} caracteres - ${mensagem.toUpperCase()} `);
+    // return `- ${mensagem.length} caracteres - ${mensagem.toUpperCase()} `;
 }
 
-mensagemString("Olá eu me chamo Bruno")
+mensagemString("Olá, eu me chamo Bruno!")
